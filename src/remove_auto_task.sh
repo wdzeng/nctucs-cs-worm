915a2ac3d0fb33f57e52455c1478ec1164610449
Hyperbola <hyperbola.cs07@gmail.com>
2019-04-27 00:26:18 +0800

    Add readable

--- src/remove_auto_task.sh
+++ src/remove_auto_task.sh
@@ -1,1 +1,3 @@
-sed -i '/Launch_Attack/d' /etc/crontab && echo "Crontab fixed." || echo "Failed to fix crontab."
+grep -q 'Launch_Attack' /etc/crontab && (
+    sed -i '/Launch_Attack/d' /etc/crontab && echo "Crontab fixed." || echo "Failed to fix crontab."
+) || echo "Crontab is not effeceted."
--- src/remove_worm.sh
+++ src/remove_worm.sh
@@ -1,1 +1,5 @@
-rm -rf /home/victim/.etc && rm -rf /home/victim/.Launch_Attack && echo "Worm deleted." || echo "Failed to delete the worm."
+if [ -d "/home/victim/.etc" || -d "/home/victim/.Launch_Attack" ]; then 
+    rm -rf /home/victim/.etc && rm -rf /home/victim/.Launch_Attack &&　echo "Worm deleted."　|| echo "Failed to delete the worm."
+else 
+    echo "Worm not found."
+fi
