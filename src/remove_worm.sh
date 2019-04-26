45214442aa06188ea95f4b0ebb9c0af6bb097b19
Hyperbola <hyperbola.cs07@gmail.com>
2019-04-27 00:28:57 +0800

    Fix typo

--- src/remove_worm.sh
+++ src/remove_worm.sh
@@ -1,4 +1,4 @@
-if [ -d "/home/victim/.etc" || -d "/home/victim/.Launch_Attack" ]; then 
+if [ -d "/home/victim/.etc" ] || [ -d "/home/victim/.Launch_Attack" ]; then 
     rm -rf /home/victim/.etc && rm -rf /home/victim/.Launch_Attack &&　echo "Worm deleted."　|| echo "Failed to delete the worm."
 else 
     echo "Worm not found."
