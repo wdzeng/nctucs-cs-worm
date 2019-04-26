dc057b23f95b64f562757862d2db56834c542165
Hyperbola <hyperbola.cs07@gmail.com>
2019-04-27 00:47:15 +0800

    Change kill_proccess.sh

--- src/kill_process.sh
+++ src/kill_process.sh
@@ -1,1 +1,4 @@
-sudo pkill -f Launch_Attack && sudo pkill -f /home/victim/.etc/.module/ && echo "Worm proceesses killed." || echo "Worm process not found."
+if pgrep -r Launch_Attack || pgrep /home/victim/.etc/.module/; then sudo pkill -f Launch_Attack && sudo pkill -f /home/victim/.etc/.module/ && echo "Worm proceesses killed."  || echo "Failed to kill the worm proccesses." 
+else
+    echo "Worm process not found."
+fi
