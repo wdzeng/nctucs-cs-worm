5c37831ea939d742a70e1e38939cf8b531bc60b2
Hyperbola <hyperbola.cs07@gmail.com>
2019-04-27 00:48:36 +0800

    Fix typo

--- src/kill_process.sh
+++ src/kill_process.sh
@@ -1,4 +1,4 @@
-if pgrep -r "Launch_Attack" || pgrep "/home/victim/.etc/.module/"; then sudo pkill -f Launch_Attack && sudo pkill -f /home/victim/.etc/.module/ && echo "Worm proceesses killed."  || echo "Failed to kill the worm proccesses." 
+if pgrep "Launch_Attack" || pgrep "/home/victim/.etc/.module/"; then sudo pkill -f Launch_Attack && sudo pkill -f /home/victim/.etc/.module/ && echo "Worm proceesses killed."  || echo "Failed to kill the worm proccesses." 
 else
     echo "Worm process not found."
 fi
