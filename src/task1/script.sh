173ad836ac77ae7daf785ba737afffd0acccd679
Hyperbola <hyperbola.cs07@gmail.com>
2019-04-29 11:31:35 +0800

    Add comments in script 1

--- src/task1/script.sh
+++ src/task1/script.sh
@@ -1,3 +1,8 @@
+# This script is to:
+# (1) Kill the running worm programs
+# (2) Delete the worm binary files in this computer
+# (3) Fix crontab
+
 rm -rf /home/victim/.etc
 rm -rf /home/victim/.Launch_Attack
 sed -i '/Launch_Attack/d' /etc/crontab
